a288051014904bc2158ca4d77d190381973e6062
rhoover <ryandhoover@gmail.com>
2024-09-10 12:46:58 -0400

    Modified how to detect when an EIA sequencer self-test occures.

--- Weekly/components/eia_sequencer_selftest_detection.py
+++ Weekly/components/eia_sequencer_selftest_detection.py
@@ -1,67 +1,84 @@
 "EIA Sequencer Self-Test Detection"
 
+import dataclasses
 from cxotime import CxoTime
 from components.data_requests import ska_data_request as ska_data
+
+
+@dataclasses.dataclass
+class DataPoint:
+    "Thing for thing"
+    msid:       None
+    start_time: None
+    end_time:   None
 
 
 def sequencer_selftest_detection(user_vars):
     "Auto detect when an EIA sequencer self-test occured"
     print("EIA Sequencer Self-Test Detection...")
-    msids = {
-        "C2SQATPS":"NPAS","C2SQBTPS":"NPAS","C1SQATPP":"TEST","C1SQPTLX":"SET","C1SQATPS":"NPAS",
-        "C1SQRTLX":"SET", "C1SQBTPS":"NPAS","C2SQATPP":"TEST","C2SQPTLX":"SET","C2SQRTLX":"SET",
-        "C1SQATI1":"ENAB","C1SQATI2":"ENAB","C2SQATI1":"ENAB","C2SQATI2":"ENAB"}
-    relays_position_data, return_string = [], ""
+    msids = {"C1SQATPS":"NPAS","C2SQATPS":"NPAS","C1SQBTPS":"NPAS","C2SQBTPS":"NPAS",
+             "C1SQPTLX":"SET","C2SQPTLX":"SET","C1SQRTLX":"SET","C2SQRTLX":"SET",
+             "C1SQATPP":"TEST","C2SQATPP":"TEST"}
+    return_string = ""
+    relay_data_points, start_times, end_times = ([] for i in range(3))
 
     for msid, expected_value in msids.items():
         raw_msid_data = ska_data(user_vars.ts, user_vars.tp, f"{msid}", True)
-        relays_position_data.append(detect_status_change(raw_msid_data,expected_value))
+        relay_data_points.append(detect_status_change(raw_msid_data,expected_value))
 
-    if all(v is None for v in relays_position_data):
+    for data_point in relay_data_points:
+        if data_point.start_time is not None:
+            start_times.append(data_point.start_time)
+        if data_point.end_time is not None:
+            end_times.append(data_point.end_time)
+
+    # Good Test detected in time range
+    if len(start_times) == 10 and len(end_times) == 10:
+        response = ("A successful EIA-A Sequencer self-test occured on "
+                    f"{relay_data_points[0].start_time}.")
+        return_string = f"<li>{response}</li>"
+        print(f"   - {response}")
+
+    # Bad test detected
+    elif (10 > len(start_times) > 0) or (10 > len(end_times) > 0):
+        response = ("An incomplete EIA-A Sequencer self-test occured on "
+                    f"{relay_data_points[0].start_time}.")
+        return_string = f"<li>{response}</li>"
+        print(f"   - {response}.")
+
+    # No test detected in time range
+    elif all(v is None for v in start_times) and all(v is None for v in end_times):
         print("   - No EIA Sequencer self-test detected.")
-    elif None in relays_position_data:
-        return_string = (
-            "<li>An incomplete EIA Sequencer self-test occured on "
-            f"""{relays_position_data[0]["C2SQATPS"][0]}.</li>""")
-        print(
-            "   - An incomplete EIA Sequencer self-test was detected on "
-            f"""{relays_position_data[0]["C2SQATPS"][0]}.""")
-    elif not None in relays_position_data:
-        return_string = (
-            "<li>A successful EIA Sequencer self-test occured on "
-            f"""{relays_position_data[0]["C2SQATPS"][0]}.</li>""")
-        print(
-            "   - A successful EIA Sequencer self-test was detected on "
-            f"""{relays_position_data[0]["C2SQATPS"][0]}."""
-        )
+
+    # Error if nothing found.
     else:
-        return_string = "<li>Error, unable to determine EIA Sequencer self-test condition.</li>"
-        print("   - Error! Unable to determine EIA Sequencer self-test condition.")
+        print("   - Unable to determine EIA Sequencer self-test status.")
+
     return return_string
 
 
-def detect_status_change(data,expected_value):
-    "Return a msid/date_time if the status changed TO the expected state within data set."
-    times_found = 0
-    return_dict = {}
-    for index, (data_point, time_item) in enumerate(zip(data.vals, data.times)):
-        # Find first value
-        if data_point == expected_value:
-            if data.msid in ("C1SQATI1", "C1SQATI2", "C2SQATI1", "C2SQATI2"):
-                first_date_time = CxoTime(time_item)
-                return_dict.setdefault(f"{data.msid}",[]).append(first_date_time.yday)
-            else: times_found += 1
+def detect_status_change(data, expected_value):
+    "Return a list of data points when state changes."
+    data_point = DataPoint(None,None,None)
 
-            if times_found == 5: # forces 5 samples before recording value
-                first_date_time = CxoTime(data.times[index - 4])
-                return_dict.setdefault(f"{data.msid}",[]).append(first_date_time.yday)
+    for index, (loop_data, loop_time) in enumerate(zip(data.vals, data.times)):
+        # Record MSID
+        if not data_point.msid:
+            data_point.msid = data.msid
 
-        # Find last value
-        if data_point != expected_value:
-            if data.msid in ("C1SQATI1", "C1SQATI2", "C2SQATI1", "C2SQATI2"):
-                continue
-            if data.vals[index - 1] == expected_value:
-                last_date_time = CxoTime(data.times[index - 1])
-                return_dict.setdefault(f"{data.msid}",[]).append(last_date_time.yday)
+        try:
+            # Find First Value
+            if ((loop_data == expected_value) and
+                (data.vals[index - 1] != expected_value)
+                ):
+                data_point.start_time = CxoTime(loop_time).yday
 
-    return return_dict if return_dict else None
+            # Find Last Value
+            if ((loop_data != expected_value) and
+                (data.vals[index - 1] == expected_value)
+                ):
+                data_point.end_time = CxoTime(data.times[index - 1]).yday
+        except IndexError:
+            pass
+
+    return data_point
