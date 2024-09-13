4018480caa8901835a76a6a56cae21cbe2b89521
Ryan Hoover <RHoover@occ.harvard.edu>
2024-09-13 13:09:43 -0400

    Combine maude/windows files. Change plot trace color

--- VC0_VC1 Slip Detector/Maude App/VC0_VC1 Slip Detector Launcher.sh
+++
@@ -1,1 +0,0 @@
-/proj/sot/ska3/flight/bin/python /share/FOT/engineering/ccdm/Tools/VC0_VC1\ Slip\ Detector/Maude\ App/"VC0_VC1_slip_detection (maude).py"
---
+++ VC0_VC1 Slip Detector/VC0_VC1 Slip Detector Launcher (GRETA).sh
@@ -0,0 +1,1 @@
+/proj/sot/ska3/flight/bin/python /share/FOT/engineering/ccdm/Tools/VC0_VC1\ Slip\ Detector/Maude\ App/"VC0_VC1_slip_detection_tool.py"
