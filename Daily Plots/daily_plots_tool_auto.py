49bdcebed8df0453d51ced07fc8a6108dd562a1d
rhoover <ryandhoover@gmail.com>
2024-09-16 09:45:59 -0400

    Adjust data rate and start/stop times

--- Daily Plots/daily_plots_tool_auto.py
+++ Daily Plots/daily_plots_tool_auto.py
@@ -4,7 +4,7 @@ Change Notes
  - Moved methods to external modules to be shared with manual run variant.
 """
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from cxotime import CxoTime
 from components.misc import cleanup
 from components.receiver_data_plot import generate_receiver_data_plots
@@ -17,15 +17,15 @@ class UserVariables:
     "User inputs object. Used to store input values."
 
     def __init__(self):
-        self.start_date = datetime.now() - timedelta(14)
-        self.end_date = datetime.now()
+        self.start_date = datetime.now(timezone.utc) - timedelta(14)
+        self.end_date = datetime.now(timezone.utc)
         self.year_start = str(self.start_date.year)
         self.doy_start = str(self.start_date.timetuple().tm_yday)
         self.year_end = str(self.end_date.year)
         self.doy_end = str(self.end_date.timetuple().tm_yday)
         self.ts = CxoTime(self.year_start+":"+self.doy_start+":00:00:00")
         self.tp = CxoTime(self.year_end+":"+self.doy_end+":23:59:59.999")
-        self.data_source = "SKA High Rate"
+        self.data_source = "SKA Abreviated"
 
 
 def main():
