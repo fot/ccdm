4e69c80a16f57d892f0b682b9fb81ce4dcb2d79e
Ryan Hoover <ryandhoover@gmail.com>
2025-07-07 14:04:38 -0400

    Updated how obc error detection appends data_points.

--- Daily Plots/components/status_report/components/obc_error_detection.py
+++ Daily Plots/components/status_report/components/obc_error_detection.py
@@ -19,15 +19,12 @@ class OBCErrorDataPoint:
 def get_obc_report_dirs(user_vars):
     "Generate list of OBC error report files"
     print(" - Building OBC Error Log report directory list...")
-    start_date= datetime.strptime(
-        f"{user_vars.year_start}:{user_vars.doy_start}:000000","%Y:%j:%H%M%S"
-        )
-    end_date= datetime.strptime(
-        f"{user_vars.year_end}:{user_vars.doy_end}:235959","%Y:%j:%H%M%S"
-        )
-    root_folder= (
-        "/share/FOT/engineering/flight_software/OBC_Error_Log_Dumps"
-        )
+    start_date= datetime.strptime(f"{user_vars.year_start}:{user_vars.doy_start}:"
+                                  "000000","%Y:%j:%H%M%S")
+    end_date=   datetime.strptime(f"{user_vars.year_end}:{user_vars.doy_end}:"
+                                  "235959","%Y:%j:%H%M%S")
+    root_folder= ("/share/FOT/engineering/flight_software/OBC_Error_Log_Dumps")
+
     full_file_list, file_list= ([] for i in range(2))
     for year_diff in range((end_date.year - start_date.year) + 1):
         year= start_date.year + year_diff
@@ -48,22 +45,27 @@ def get_obc_report_dirs(user_vars):
     return file_list
 
 
-def get_obc_error_reports(file_list):
+def get_obc_error_reports(file_list, user_vars):
     "Parse OBC error reports into data"
     print(" - Parsing OBC Error reports...")
     report_data= []
 
     for file_dir in tqdm(file_list, bar_format= "{l_bar}{bar:20}{r_bar}{bar:-10b}"):
-        report_data.extend(parse_obc_report(file_dir))
+        report_data.extend(parse_obc_report(file_dir, user_vars))
 
     return report_data
 
 
-def parse_obc_report(file_dir):
+def parse_obc_report(file_dir, user_vars):
     """
     Description: Parse OBC error report
     """
     data_list= []
+    start_date= datetime.strptime(f"{user_vars.year_start}:{user_vars.doy_start}:"
+                                  "000000","%Y:%j:%H%M%S")
+    end_date=   datetime.strptime(f"{user_vars.year_end}:{user_vars.doy_end}:"
+                                  "235959","%Y:%j:%H%M%S")
+
     with open(file_dir, 'r', encoding="utf-8") as obc_error_log:
         for line in obc_error_log:
             parsed= line.split()
@@ -79,11 +81,12 @@ def parse_obc_report(file_dir):
                     except IndexError:
                         error= f"{parsed[8]}"
                     data_point.error= error
-                    data_list.append(data_point)
+                    if start_date <= full_date <= end_date:
+                        data_list.append(data_point)
             except IndexError:
                 pass
 
-    return data_list if data_list else None
+    return data_list
 
 
 def write_obc_error_report(user_vars, file, report_data):
@@ -136,5 +139,5 @@ def obc_error_detection(user_vars, file):
     """
     print("\nAdding OBC Error Data...")
     file_list= get_obc_report_dirs(user_vars)
-    report_data= get_obc_error_reports(file_list)
+    report_data= get_obc_error_reports(file_list, user_vars)
     write_obc_error_report(user_vars, file, report_data)
